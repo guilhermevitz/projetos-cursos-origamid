361902dc8d86563dc3cd3232235d9d3a67020f61
oguizito <guilhermevitz@outlook.com>
2022-11-18 20:48:06 -0300

    add js galeria bicicletas

--- Projeto Bikcraft completo/js/script.js
+++ Projeto Bikcraft completo/js/script.js
@@ -45,3 +45,23 @@ function eventosPerguntas(pergunta) {
 
 perguntas.forEach(eventosPerguntas);
 
+
+// GALERIA DE BICICLETAS
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
