0d905db92ae9dcd2ceb08b835a3879789a7b9e73
oguizito <guilhermevitz@outlook.com>
2022-11-20 04:09:55 -0300

    primeiro commit javascript ES6

---
+++ JavaScript Completo ES6/tipos-de-dados/script.js
@@ -0,0 +1,28 @@
+// Declare uma variável contendo uma string
+
+let curso = 'js';
+
+// Declare uma variável contendo um número dentro de uma string
+
+let modulo = '1';
+
+// Declare uma variável com a sua idade
+
+let idade = 32;
+
+// Declare duas variáveis, uma com seu nome 
+// e outra com seu sobrenome e some as mesmas
+let nome = 'Guilherme';
+let sobrenome = 'Cruz';
+let nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+
+let frase = "It's Time";
+
+
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(typeof nome);
---
+++ javascript-completo-es6/variaveis/script.js
@@ -0,0 +1,22 @@
+// Declarar uma variável com o seu nome
+
+// Declarar uma variável com a sua idade
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+// Atribuir valor a sua comida favorita
+
+// Declarar 5 variáveis diferentes sem valores
+
+var nome = 'Guilherme';
+var idade = 32 + 'anos';
+
+
+var comidaFavorita;
+comidaFavorita = 'Macarrão';
+
+
+var carro, iphone, macbook, casa, desenvolvedor;
+
+console.log(nome, idade, comidaFavorita,)
