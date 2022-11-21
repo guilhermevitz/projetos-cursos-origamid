f78f7eb1b323f62757252ac0403a9305bf00f588
oguizito <guilhermevitz@outlook.com>
2022-11-21 05:14:42 -0300

    números e operadores

---
+++ javascript-completo-es6/numeros-e-operadores/script.js
@@ -0,0 +1,51 @@
+let soma = 10 + 5 + 20;
+let divisao = 300 / 2;
+let modulo = 3109033 % 5;
+
+console.log(modulo);
+
+let testeNan = 'Isso é 100' / 2;
+console.log(testeNan);
+
+let soma1 = 10 + 10 + 20 + 30 * 5 * 4 /2 + 10;
+console.log(soma1);
+
+
+let x = 6;
+console.log(++x);
+
+let idade = '32'; //string proposital //
+let somaIdade = 6;
+
+console.log(+idade + somaIdade);
+
+// EXERCÍCIO
+
+// Qual o resultado da seguinte expressão?
+let total = 10 + 5 * 2 / 2 + 20;
+total = 35;
+
+// Crie duas expressões que retornem NaN //
+
+let altura = 'tenho 1,90'- 1;
+let idade1 = 'tenho 60' / 2;
+console.log(altura, idade1);
+
+
+// Somar a string '200' com o número 50 e retornar 250
+
+let soma2 = +'200' + 50;
+console.log(soma2);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+let y = 8;
+console.log(++y);
+
+// Como dividir o peso por 2?
+
+let numero = +'90'/ 2;
+let unidade = 'kg';
+let peso = numero + unidade; // '80kg'
+
+console.log(peso);
