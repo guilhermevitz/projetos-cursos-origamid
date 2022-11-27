215eac54e3901463a1cddedacfcc2d91991660fc
oguizito <guilhermevitz@outlook.com>
2022-11-27 10:38:57 -0300

    tudo é objeto

---
+++ javascript-completo-es6/tudo-e-objeto/script.js
@@ -0,0 +1,16 @@
+
+// nomeie 3 propriedades ou métodos de strings
+let jogo = 'Genshim';
+//fixed
+//length
+//slice 
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+let btn = document.querySelector('.btn');
+// addEventListener
+// appendChild
+// id
+// outerHTML
+// innerHtml
+
+
